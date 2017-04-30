74b2496f1eadca5252a83627e43637a5704a6209
xafero <georg.berendt@outlook.com>
2017-05-01 00:22:40 +0200

    Added an API for patching

---
+++ NetInject.API/IInvocationHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace NetInject.API
+{
+    /// <summary>
+    /// The entry point to patch methods
+    /// </summary>
+    public interface IInvocationHandler
+    {
+        /// <summary>
+        /// Patches a method invocation on an instance
+        /// </summary>
+        /// <param name="real">the used object</param>
+        /// <param name="method">the invoked method</param>
+        /// <param name="args">given arguments</param>
+        /// <returns>recalculated result</returns>
+        /// <exception cref="System.NotImplementedException">if not interesting</exception>
+        object Invoke(object real, MethodInfo method, params object[] args);
+    }
+}

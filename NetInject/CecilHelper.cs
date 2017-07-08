ef14837ce624e45fc03eb3a1f3d8c1c9797d9110
xafero <georg.berendt@outlook.com>
2017-07-08 19:23:00 +0200

    Added cecil helper

---
+++ NetInject/CecilHelper.cs
@@ -0,0 +1,18 @@
+﻿using Mono.Cecil;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NetInject
+{
+    static class CecilHelper
+    {
+        public static IEnumerable<TypeDefinition> GetAllTypes(this AssemblyDefinition ass)
+            => ass.Modules.SelectMany(m => m.GetAllTypes());
+
+        public static IEnumerable<TypeDefinition> GetAllTypes(this ModuleDefinition mod)
+            => mod.Types.SelectMany(t => t.GetAllTypes());
+
+        public static IEnumerable<TypeDefinition> GetAllTypes(this TypeDefinition type)
+            => (new[] { type }).Concat(type.NestedTypes.SelectMany(t => t.GetAllTypes()));
+    }
+}

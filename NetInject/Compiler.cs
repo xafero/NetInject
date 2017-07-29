0aba00dffa70907780b850f35fa80729da11a6fb
xafero <georg.berendt@uni-oldenburg.de>
2017-07-29 23:49:26 +0200

    Errors will be logged

--- NetInject/Compiler.cs
+++ NetInject/Compiler.cs
@@ -1,10 +1,13 @@
-﻿using System.CodeDom.Compiler;
+﻿using log4net;
+using System.CodeDom.Compiler;
 using System.Reflection;
 
 namespace NetInject
 {
     public static class Compiler
     {
+        static readonly ILog log = LogManager.GetLogger(typeof(Compiler));
+
         public static Assembly CreateAssembly(string name, string[] files, string[] refs = null)
         {
             var provider = CodeDomProvider.CreateProvider("CSharp");
@@ -20,6 +23,9 @@ namespace NetInject
             if (refs?.Length >= 1)
                 options.ReferencedAssemblies.AddRange(refs);
             var res = provider.CompileAssemblyFromFile(options, files);
+            if (res.Errors.HasErrors)
+                foreach (var err in res.Errors)
+                    log.ErrorFormat("{0}", err);
             return res.CompiledAssembly;
         }
     }

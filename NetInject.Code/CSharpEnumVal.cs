2fb121b7717dc0a6f6e3597636dfea1b24696b9a
xafero <georg.berendt@uni-oldenburg.de>
2017-07-30 02:36:25 +0200

    Added enum and delegate code writing

---
+++ NetInject.Code/CSharpEnumVal.cs
@@ -0,0 +1,14 @@
+﻿namespace NetInject.Code
+{
+    public class CSharpEnumVal
+    {
+        public string Name { get; }
+
+        public CSharpEnumVal(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString() => $"{Name}";
+    }
+}

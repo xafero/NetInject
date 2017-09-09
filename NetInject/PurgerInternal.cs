8a22f8fe12bcf6e2432a1b61e292ba21c7bd6ce5
xafero <georg.berendt@uni-oldenburg.de>
2017-09-09 03:14:29 +0200

    Native is replaced with NOP

--- NetInject/PurgerInternal.cs
+++ NetInject/PurgerInternal.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using AMA = System.Reflection.AssemblyMetadataAttribute;
 using System;
 using System.Diagnostics;
+using Mono.Cecil.Cil;
 
 namespace NetInject
 {
@@ -36,7 +37,6 @@ namespace NetInject
 
         private void Rewrite(AssemblyDefinition ass, ModuleReference modRef, AssemblyDefinition insAss)
         {
-            // TODO: Inject natives?
             var pinvokes = ass.GetAllTypes().SelectMany(t => t.Methods).Where(
                 m => m.HasPInvokeInfo && m.PInvokeInfo.Module == modRef).ToArray();
             var oldTypes = new HashSet<TypeReference>();
@@ -45,9 +45,23 @@ namespace NetInject
                 var typRefs = pinvoke.GetDistinctTypes().ToDictionary(k => k,
                     v => insAss.GetAllTypes().FirstOrDefault(t => t.Match(v)));
                 pinvoke.PatchTypes(typRefs, t => oldTypes.Add(t));
-                // var type = pinvoke.DeclaringType; type.Methods.Remove(pinvoke);
                 pinvoke.RemovePInvoke();
+                var type = pinvoke.DeclaringType;
+                type.Methods.Remove(pinvoke);
             }
+            foreach (var meth in ass.GetAllTypes().SelectMany(t => t.Methods).Where(
+                m => !m.HasPInvokeInfo && m.HasBody))
+                foreach (var instr in meth.Body.Instructions)
+                {
+                    if (instr.OpCode != OpCodes.Call)
+                        continue;
+                    var methRef = instr.Operand as MethodReference;
+                    if (methRef == null || !pinvokes.Contains(methRef))
+                        continue;
+                    // TODO: Replace correctly!
+                    instr.OpCode = OpCodes.Nop;
+                    instr.Operand = null;
+                }
             foreach (var oldType in oldTypes.OfType<TypeDefinition>())
             {
                 var module = oldType.Module;

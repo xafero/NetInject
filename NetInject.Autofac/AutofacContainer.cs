d0793c53eacc1167851f1379fb350123b8a372fe
xafero <georg.berendt@outlook.com>
2017-09-14 22:53:09 +0200

    Added singletons for registered types

--- NetInject.Autofac/AutofacContainer.cs
+++ NetInject.Autofac/AutofacContainer.cs
@@ -3,15 +3,19 @@ using Autofac.Core.Registration;
 using NetInject.API;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
-
 using static NetInject.Autofac.DynamicLoad;
+using IContainer = Autofac.IContainer;
 
 namespace NetInject.Autofac
 {
     public class AutofacContainer : IVessel
     {
+        private const string ServiceSuffix = "Service";
+        private const StringComparison cmp = StringComparison.InvariantCulture;
+
         private IContainer Container { get; }
 
         public AutofacContainer()
@@ -25,15 +29,18 @@ namespace NetInject.Autofac
             });
             asses.AddRange(AppDomain.CurrentDomain.GetAssemblies());
             asses.AddRange(GetSameDirectoryAssemblies(GetType()));
-            var cmp = StringComparison.InvariantCulture;
             var builder = new ContainerBuilder();
             foreach (var ass in asses.Where(a => a != null).Distinct())
                 try
                 {
                     ass.GetTypes();
-                    builder.RegisterAssemblyTypes(ass)
-                           .Where(t => t.Name.EndsWith("Service", cmp))
-                           .AsImplementedInterfaces();
+                    builder.RegisterAssemblyTypes(ass).Where(IsConventional)
+                        .AsImplementedInterfaces().SingleInstance();
+                    builder.RegisterAssemblyTypes(ass).Where(HasName)
+                        .Named<Func<Type, string>>(GetName).SingleInstance();
+                    // .ExternallyOwned()
+                    // .InstancePerLifetimeScope()
+                    // .AsSelf()
                 }
                 catch (ReflectionTypeLoadException)
                 {
@@ -42,6 +49,15 @@ namespace NetInject.Autofac
                 }
             Container = builder.Build();
         }
+
+        private static string GetName(Type t)
+            => t.GetCustomAttribute<DescriptionAttribute>().Description;
+
+        private static bool HasName(Type t)
+            => t.Name.EndsWith(ServiceSuffix, cmp) && t.GetCustomAttribute<DescriptionAttribute>(false) != null;
+
+        private static bool IsConventional(Type t)
+            => t.Name.EndsWith(ServiceSuffix, cmp) && t.GetCustomAttributes(false).Length == 0;
 
         public T Resolve<T>() where T : class
         {
